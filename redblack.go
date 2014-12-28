559834b3a489935723a2170c5e4fdde996111563
Nick Pankow <nickpankow@nicks-mbp.gateway.pace.com>
2014-12-27 16:22:05 -0800

    Added methods and testing methods for rotate left and right operations.

--- redblack.go
+++ redblack.go
@@ -4,24 +4,108 @@ import (
 	"fmt"
 )
 
+type RedBlackTree struct {
+	Root 		*RedBlackNode
+}
+
 type RedBlackNode struct {
-	value		interface{}
+	Value 		int
+	Data 		interface{}
 	redblack	bool
+	parent		*RedBlackNode
 	left		*RedBlackNode
 	right		*RedBlackNode
 }
 
-func (rb RedBlackNode) Insert(element interface{}) (*RedBlackNode) {
-	fmt.Println("Insert ", element)
-	return nil
+func NewTree() (*RedBlackTree){
+	return new(RedBlackTree)
 }
 
-func (rb RedBlackNode) Delete(element interface{}) (*RedBlackNode) {
+func (rb *RedBlackTree) Insert(element *RedBlackNode) {
+	fmt.Println("Insert ", element)
+	if rb.Root == nil {
+		rb.Root = element
+	} else {
+		return
+	}
+}
+
+// func tranverse(start *RedBlackNode, value int) (*RedBlackNode) {
+// 	if start.Value > value {
+
+// 	} else {
+
+// 	}
+// }
+
+func leftRotate(t *RedBlackTree, x *RedBlackNode) {
+	var y *RedBlackNode
+	y = x.right
+	// Turn y's left sub-tree into x's right sub-tree
+	x.right = y.left
+	if y.left != nil {
+		y.left.parent = x
+	}
+	// y's new parent was x's parent
+	y.parent = x.parent
+	// Set parent to point to y instead of x
+	
+	// First see whether we're at the root
+	if x.parent == nil {
+		t.Root = y
+	} else {
+		if (x == x.parent.left) {
+			// x was on the left of its parent
+			x.parent.left = y
+		} else {
+			// x must have been on the right
+			x.parent.right = y
+		}
+	}
+	// Finally, put x on y's left
+	y.SetLeftChild(x)
+}
+
+func rightRotate(t *RedBlackTree, y *RedBlackNode) {
+	var x *RedBlackNode
+	x = y.left
+
+	y.left = x.right
+	if x.right != nil {
+		x.right.parent = y
+	}
+
+	x.parent = y.parent
+	if y.parent == nil {
+		t.Root = x
+	} else {
+		if (y == y.parent.right) {
+			y.parent.right = x
+		} else {
+			y.parent.left = x
+		}
+	}
+	x.SetRightChild(y)
+}
+
+func (rb RedBlackTree) Delete(element int) (*RedBlackNode) {
 	fmt.Println("Delete ", element)
 	return nil
 }
 
-func (rb RedBlackNode) Find(value interface{}) (*RedBlackNode){
+func (rb RedBlackTree) Find(value int) (*RedBlackNode){
 	fmt.Println("Find: ", value)
 	return nil
+}
+
+/** Node Child Methods **/
+
+func (rbn *RedBlackNode) SetLeftChild(x *RedBlackNode) {
+	rbn.left = x
+	x.parent = rbn
+}
+
+func (rbn *RedBlackNode) SetRightChild(x *RedBlackNode) {
+	rbn.right = x
+	x.parent = rbn
 }
--- redblack_test.go
+++ redblack_test.go
@@ -5,11 +5,24 @@ import (
 )
 
 func TestNewTree(t *testing.T) {
-	t.Error("New tree not implemented")
+	rbTree := NewTree()
+	if (rbTree == nil || rbTree.Root != nil) {
+		t.Error("New tree not initialized.")		
+	}
 }
 
 func TestInsert(t *testing.T) {
-	t.Error("Insert not implemented")
+	rbTree := NewTree()
+
+	rbNode := RedBlackNode{}
+	rbNode.Value = 50
+
+	rbTree.Insert(&rbNode)
+
+	if rbTree.Root == nil || rbTree.Root.Value != 50 {
+		t.Error("Root node value mismatch.  Root: ", rbTree.Root)
+	}
+	// t.Error("Insert not implemented")
 }
 
 func TestDelete(t *testing.T) {
@@ -18,4 +31,76 @@ func TestDelete(t *testing.T) {
 
 func TestFind(t *testing.T) {
 	t.Error("Find not implemented")
+}
+
+func TestRotateLeft(t *testing.T) {
+	// Test with element as the root
+	x := &RedBlackNode{ 0, "x", false, nil, nil, nil}
+	y := &RedBlackNode{ 1, "y", false, nil, nil, nil}
+	a := &RedBlackNode{3, "A", false, nil, nil, nil}
+	b := &RedBlackNode{4, "B", false, nil, nil, nil}
+	c := &RedBlackNode{2, "C", false, nil, nil, nil}
+
+	x.SetLeftChild(a)
+	x.SetRightChild(y)
+	y.SetLeftChild(b)
+	y.SetRightChild(c)
+
+	tree := NewTree()
+	tree.Root = x
+
+	leftRotate(tree, x)
+	if tree.Root != y || y.left != x || x.right != b || x.left != a || y.right != c {
+		t.Error("Incorrect Left-Rotate result")
+	}
+
+	// Test with element not as the root
+	z := &RedBlackNode{ 5, "z", false, nil, nil, nil}
+	x.SetLeftChild(a)
+	x.SetRightChild(y)
+	y.SetLeftChild(b)
+	y.SetRightChild(c)
+	z.SetLeftChild(x)
+
+	tree.Root = z
+	leftRotate(tree, x)
+	if tree.Root != z || y.left != x || x.right != b || x.left != a || y.right != c {
+		t.Error("Incorrect Left-Rotate result")
+	}
+}
+
+func TestRotateRight(t *testing.T) {
+	// Test with element as the root
+	x := &RedBlackNode{ 0, "x", false, nil, nil, nil}
+	y := &RedBlackNode{ 1, "y", false, nil, nil, nil}
+	a := &RedBlackNode{3, "A", false, nil, nil, nil}
+	b := &RedBlackNode{4, "B", false, nil, nil, nil}
+	c := &RedBlackNode{2, "C", false, nil, nil, nil}
+
+	y.SetLeftChild(x)
+	y.SetRightChild(c)
+	x.SetLeftChild(a)
+	x.SetRightChild(b)
+
+	tree := NewTree()
+	tree.Root = y
+
+	rightRotate(tree, y)
+	if tree.Root != x || x.left != a || x.right != y || y.left != b || y.right != c {
+		t.Error("Incorrect Right-Rotate result")
+	}
+
+	// Test with element not as the root
+	z := &RedBlackNode{ 5, "z", false, nil, nil, nil}
+	y.SetLeftChild(x)
+	y.SetRightChild(c)
+	x.SetLeftChild(a)
+	x.SetRightChild(b)
+	z.SetRightChild(y)
+
+	tree.Root = z
+	rightRotate(tree, y)
+	if tree.Root != z || x.left != a || x.right != y || y.left != b || y.right != c {
+		t.Error("Incorrect Right-Rotate result")
+	}
 }
